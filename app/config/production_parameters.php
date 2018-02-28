70f116ce3998e2b6bced10a935ed14ef19ca96a2
Pablo Díaz <pablo5_diaz@hotmail.com>
2018-02-28 11:46:17 -0600

    remove invalid char

--- app/config/production_parameters.php
+++ app/config/production_parameters.php
@@ -2,7 +2,6 @@
     $container->setParameter('secret', getenv('SECRET'));
     $container->setParameter('locale', 'es');
     $container->setParameter('api_email_key',getenv('API_EMAIL'));
-    );
     $container->setParameter('mailer_transport', 'smtp');
     $container->setParameter('mailer_host', getenv('MAILER_HOST'));
     $container->setParameter('mailer_user', getenv('MAILER_USER'));
