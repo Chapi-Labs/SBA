3612b8846b66afc6f318a9d5ba531b0546fc1e21
Pablo Díaz <pablo5_diaz@hotmail.com>
2018-02-28 11:31:47 -0600

    add missing parameters

--- app/config/production_parameters.php
+++ app/config/production_parameters.php
@@ -2,3 +2,8 @@
     $container->setParameter('secret', getenv('SECRET'));
     $container->setParameter('locale', 'es');
     $container->setParameter('api_email_key',getenv('API_EMAIL'));
+    );
+    $container->setParameter('mailer_transport', 'smtp');
+    $container->setParameter('mailer_host', getenv('MAILER_HOST'));
+    $container->setParameter('mailer_user', getenv('MAILER_USER'));
+    $container->setParameter('mailer_password', getenv('MAILER_PASSWORD'));
